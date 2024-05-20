2676fd469c5330b6b746a90e2ad21394e7fc87a9
matson83 <matsonjunior194@gmail.com>
2024-05-20 00:35:28 -0300

    Preparação da apresentação

--- OcrExample.py
+++ OcrExample.py
@@ -2,25 +2,24 @@ import google.generativeai as genai
 import whisper
 
 model = whisper.load_model("tiny")
-audio_path = "C:/Users/matso/Documents/OCR/OCR/gravando2.mp3"
+audio_path = "C:/Users/matso/Documents/OCR/OCR/gravação1.mp3"
 
 API_KEY = 'AIzaSyCGW81sQiyNEp5ZjkreCU5l0rijOTBexAE'
 genai.configure(api_key=API_KEY)
-
 
 def pergunta():
     result = model.transcribe(audio_path)
     transcript = result["text"]
 
     modelo = genai.GenerativeModel('gemini-pro')
-    pergunta = ('qual o tema desta fala? \n'
-                'Mostre as fontes : \n'
-                'Mostre quem é citado nessa fala :')
+    pergunta = ('Qual o tema do discurso? \n'
+                'Os dados mostrados são fake news? (Responda com sim ou não) \n'
+                'Se não são fake news, mostre esses dados e as possíveis fontes que confirmam a veracidade: (As fontes não precisam ser links específicos, mas que levem a uma base confiável desses dados citados)'
+                'Quem é citado no discurso:')
     transcricao = pergunta + "\n" + transcript
     resposta = modelo.generate_content(transcricao)
 
-    print(f"Resposta: \n{resposta.text}")
+    print(f"Resposta: \n{resposta.text}\n")
     print("Transcrição: \n{}".format(transcript.replace('. ', '.\n').replace('? ', '?\n').replace('! ', '!\n')))
-
 
 pergunta()
